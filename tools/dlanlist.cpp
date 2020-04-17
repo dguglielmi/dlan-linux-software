b3e892754471bb52a0dcd7c38406af626d3ff866
David GUGLIELMI <david.guglielmi@gmail.com>
2020-04-17 12:40:36 +0200

    dlanlist.cpp: use early return

--- tools/dlanlist.cpp
+++ tools/dlanlist.cpp
@@ -48,49 +48,47 @@ int main(int argc, char** argv)
            "List dlan devices found on the given network interfaces,\n"
            "or on all interfaces, if none where given.\n"
            "\n");
-  }
-  else
-  {
-    CPacketInterfaceLinux pktIfc(setIfcNames);
-    CDlanMonitor m(pktIfc);
-    CPacketTaskRunner(pktIfc, m).Run(1000, MSCounter());
- 
-    if(m.GetLocalDevices().empty())
-    {
-      printf("no devices found\n");
-      result = 1;
-    }
-    else
-    {
-      printf("Type    MAC address        Mbps TX/RX       Version/Product\n");
-      for(CDlanLocalDeviceList::const_iterator itLocal = m.GetLocalDevices().begin();
-        (itLocal != m.GetLocalDevices().end()); ++itLocal)
-      {
-        printf("local   %s  ---.-- / ---.--  %s\n", 
-          itLocal->GetAddress().ToString().c_str(), 
-          VersionProductString(*itLocal).c_str());
 
-        for(CDlanRemoteDeviceList::const_iterator itRemote = itLocal->GetRemoteDevices().begin();
-          (itRemote != itLocal->GetRemoteDevices().end()); ++itRemote)
-        {
-          if(itRemote->GetType() == CDlanDevice::eThunderbolt)
-          {
-            printf("remote  %s  %6.02f / %6.02f  %s\n", 
-              itRemote->GetAddress().ToString().c_str(), 
-              itRemote->GetTxRate(),
-              itRemote->GetRxRate(),
-              itRemote->GetVersion().c_str());
-          }
-          else
-          {
-            printf("remote  %s  %6.02f / ---.--  %s\n", 
-              itRemote->GetAddress().ToString().c_str(), 
-              itRemote->GetTxRate(),
-              VersionProductString(*itRemote).c_str());
-          }
-        }
+    return result;
+  }
+
+ CPacketInterfaceLinux pktIfc(setIfcNames);
+ CDlanMonitor m(pktIfc);
+ CPacketTaskRunner(pktIfc, m).Run(1000, MSCounter());
+
+ if(m.GetLocalDevices().empty())
+ {
+   printf("no devices found\n");
+   return 1;
+ }
+
+  printf("Type    MAC address        Mbps TX/RX       Version/Product\n");
+  for(CDlanLocalDeviceList::const_iterator itLocal = m.GetLocalDevices().begin();
+    (itLocal != m.GetLocalDevices().end()); ++itLocal)
+  {
+    printf("local   %s  ---.-- / ---.--  %s\n",
+      itLocal->GetAddress().ToString().c_str(),
+      VersionProductString(*itLocal).c_str());
+ 
+    for(CDlanRemoteDeviceList::const_iterator itRemote = itLocal->GetRemoteDevices().begin();
+      (itRemote != itLocal->GetRemoteDevices().end()); ++itRemote)
+    {
+      if(itRemote->GetType() == CDlanDevice::eThunderbolt)
+      {
+        printf("remote  %s  %6.02f / %6.02f  %s\n",
+          itRemote->GetAddress().ToString().c_str(),
+          itRemote->GetTxRate(),
+          itRemote->GetRxRate(),
+          itRemote->GetVersion().c_str());
       }
-    }    
+      else
+      {
+        printf("remote  %s  %6.02f / ---.--  %s\n",
+          itRemote->GetAddress().ToString().c_str(),
+          itRemote->GetTxRate(),
+          VersionProductString(*itRemote).c_str());
+      }
+    }
   }
   
   return result;
